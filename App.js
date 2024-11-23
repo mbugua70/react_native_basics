9df867f65e481e198c4981864c5559a03e126d4c
Code MBU <johnmbugua23code@gmail.com>
2024-11-23 12:46:55 +0300

    react navigation

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
-import CategoriesScreen from './screens/CategoriesScreen';
+import * as SystemUI from "expo-system-ui";
+import CategoriesScreen from "./screens/CategoriesScreen";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    SystemUI.getBackgroundColorAsync("#24180f");
+  }, []);
   return (
     <>
+      <StatusBar style="light" />
       <SafeAreaView>
-        <CategoriesScreen/>
+        <CategoriesScreen />
       </SafeAreaView>
     </>
   );
--- components/CategoryGridItem.js
+++ components/CategoryGridItem.js
@@ -1,48 +1,64 @@
-import { View, Text, Pressable, StyleSheet, ScrollView } from 'react-native'
+import {
+  View,
+  Text,
+  Pressable,
+  StyleSheet,
+  ScrollView,
+  Platform,
+} from "react-native";
 
-
-const CategoryGridItem = ({title, color}) => {
+const CategoryGridItem = ({ title, color }) => {
   return (
-    <View style={[styles.gridNavItem, {backgroundColor: color}]}>
-      <Pressable style={styles.buttonContainer}>
-        <View style={styles.innerGridContainer}>
-            <Text style="">
-                {title}
-            </Text>
+    <View style={styles.gridNavItem}>
+      <Pressable
+        android_ripple={{ color: "#ccc" }}
+        style={({ pressed }) => [
+          styles.buttonContainer,
+          pressed ? styles.pressedButton : null,
+        ]}
+      >
+        <View style={[styles.innerGridContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default CategoryGridItem
+export default CategoryGridItem;
 
-const  styles = StyleSheet.create({
-    gridNavItem: {
-        flex: 1,
-        margin: 18,
-        borderRadius: 8,
-        elevation: 6,
-        height: 150,
-        shadowRadius: 8,
-        shadowColor: "black",
-        shadowOffset: {width: 0, height: 2},
-        shadowOpacity: 0.35
-    },
+const styles = StyleSheet.create({
+  gridNavItem: {
+    flex: 1,
+    margin: 18,
+    borderRadius: 8,
+    elevation: 6,
+    height: 150,
+    shadowRadius: 8,
+    shadowColor: "black",
+    backgroundColor: "white",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.35,
+    overflow: Platform.OS === "android" ? "hidden" : "visible",
+  },
 
-    buttonContainer: {
-      flex: 1
-    },
+  buttonContainer: {
+    flex: 1,
+  },
+  pressedButton: {
+    opacity: 0.5,
+  },
 
-    innerGridContainer: {
-      flex: 1,
-      padding: 16,
-      justifyContent: "center",
-      alignItems: "center",
-    },
+  innerGridContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
 
-    title: {
-        fontWeight: 'bold',
-        fontSize: 24
-    }
-})
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
